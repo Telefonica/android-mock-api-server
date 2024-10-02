52b0fbdb6b2dbf2c31045b13378aeff4897a1085
Juan García <juan.garciadieguez@telefonica.com>
2024-10-02 10:20:42 +0200

    remove suspend keyword from getBaseUrl function #ANDROID-15254

--- app/src/main/java/com/telefonica/mocks/domain/backend/InitBackendUrl.kt
+++ app/src/main/java/com/telefonica/mocks/domain/backend/InitBackendUrl.kt
@@ -4,18 +4,14 @@ import com.telefonica.mock.MockHelper
 import com.telefonica.mocks.BuildConfig
 import com.telefonica.mocks.common.Environment
 import com.telefonica.mocks.data.backend.BackendRepository
-import com.telefonica.mocks.di.IoDispatcher
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 open class InitBackendUrl @Inject constructor(
     private val mockHelper: MockHelper,
     private val backendRepository: BackendRepository,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher,
 ) {
 
-    open suspend operator fun invoke() = withContext(dispatcher) {
+    open suspend operator fun invoke() {
         backendRepository.backendUrl = when (BuildConfig.DEFAULT_ENVIRONMENT == Environment.DEMO) {
             true -> mockHelper.getBaseUrl()
             false -> BuildConfig.DEFAULT_ENVIRONMENT.baseUrl
